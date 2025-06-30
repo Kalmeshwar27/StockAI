e77bbc42b7ab97ce3190d633f906c1cb19d344b0
Kalmeshwar27 <kalmeshwar.fullstackdev@gmail.com>
2025-06-30 14:05:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,5 +73,5 @@ def get_articles_by_tag_id(tag_id):
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5500))
     app.run(host="0.0.0.0", port=port)
